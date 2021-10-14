90c7785475d0d572375b37f42c5ce4fa06486910
Max Fry <maxcfry@outlook.com>
2021-10-14 17:31:56 +0100

    Step 15 Work In Progress

--- app.rb
+++ app.rb
@@ -30,6 +30,16 @@ class BookmarkManager < Sinatra::Base
      redirect '/bookmarks'
   end 
 
+  get '/bookmarks/:id/edit' do
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])  
+    redirect('/bookmarks')
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
 
